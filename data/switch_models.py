9de31953142d54a1e518d3065e9fd403cf41d804
cscarpa <caio.scarpa@outlook.com.br>
2025-02-21 16:21:14 -0300

    Update switch_models.py

--- data/switch_models.py
+++ data/switch_models.py
@@ -2,168 +2,310 @@ SWITCH_MODELS = {
     "Meraki MS130": {
         "MS130-8P": {
             "Type": "L2",
-            "Access Ports": 8,
-            "Port Speed": [
-                {"Ports": 8, "Speed (Gbps)": [1]}
+            "Access": [
+                {"Ports": 8, "Speed": [1]}
             ],
             "PoE Type": "PoE+",
-            "PoE Budget (W)": 120,
-            "Uplink Speed (Gbps)": [1],
-            "Uplink Ports": 2,
+            "PoE Budget": 120,
+            "Uplink": [
+                {"Ports": 2, "Speed": [1]}
+            ],
+            "Switching Capacity": 20,
+            "Stacking": "No",
             "SKU": "MS130-8P-HW",
-            "Switching Capacity (Gbps)": 20,
+            "License": [
+                {"Enterprise": "LIC-MS130-CMPT-xY", "Advanced": "LIC-MS130-CMPTA-xY"}
+            ],
             "Datasheet": "https://documentation.meraki.com/MS/MS_Overview_and_Specifications/MS130_Datasheet",
             "Installation Guide": "https://documentation.meraki.com/MS/MS_Installation_Guides/MS130_Series_Installation_Guide"
         },
         "MS130-8X": {
             "Type": "L2",
-            "Access Ports": 8,
-            "Port Speed": [
-                {"Ports": 2, "Speed (Gbps)": [2.5]}, 
-                {"Ports": 6, "Speed (Gbps)": [1]}
+            "Access": [
+                {"Ports": 2, "Speed": [2.5]}, 
+                {"Ports": 6, "Speed": [1]}
             ],
             "PoE Type": "PoE+",
-            "PoE Budget (W)": 120,
-            "Uplink Speed (Gbps)": [10],
-            "Uplink Ports": 2,
+            "PoE Budget": 120,
+            "Uplink": [
+                {"Ports": 2, "Speed": [10]}
+            ],
+            "Switching Capacity": 62,
+            "Stacking": "No",
             "SKU": "MS130-8X-HW",
-            "Switching Capacity (Gbps)": 62,
+            "License": [
+                {"Enterprise": "LIC-MS130-CMPT-xY", "Advanced": "LIC-MS130-CMPTA-xY"}
+            ],
             "Datasheet": "https://documentation.meraki.com/MS/MS_Overview_and_Specifications/MS130_Datasheet",
             "Installation Guide": "https://documentation.meraki.com/MS/MS_Installation_Guides/MS130_Series_Installation_Guide"
         },
         "MS130-12X": {
             "Type": "L2",
-            "Access Ports": 12,
-            "Port Speed": [
-                {"Ports": 4, "Speed (Gbps)": [2.5]}, 
-                {"Ports": 8, "Speed (Gbps)": [1]}
+            "Access": [
+                {"Ports": 4, "Speed": [2.5]}, 
+                {"Ports": 8, "Speed": [1]}
             ],
             "PoE Type": "PoE+",
-            "PoE Budget (W)": 240,
-            "Uplink Speed (Gbps)": [10],
-            "Uplink Ports": 2,
+            "PoE Budget": 240,
+            "Uplink": [
+                {"Ports": 2, "Speed": [10]}
+            ],
+            "Switching Capacity": 76,
+            "Stacking": "No",
             "SKU": "MS130-12X-HW",
-            "Switching Capacity (Gbps)": 76,
+            "License": [
+                {"Enterprise": "LIC-MS130-CMPT-xY", "Advanced": "LIC-MS130-CMPTA-xY"}
+            ],
             "Datasheet": "https://documentation.meraki.com/MS/MS_Overview_and_Specifications/MS130_Datasheet",
             "Installation Guide": "https://documentation.meraki.com/MS/MS_Installation_Guides/MS130_Series_Installation_Guide"
         },
         "MS130-24X": {
             "Type": "L2",
-            "Access Ports": 24,
-            "Port Speed": [
-                {"Ports": 6, "Speed (Gbps)": [2.5]}, 
-                {"Ports": 18, "Speed (Gbps)": [1]}
+            "Access": [
+                {"Ports": 6, "Speed": [2.5]}, 
+                {"Ports": 18, "Speed": [1]}
             ],
             "PoE Type": "PoE+",
-            "PoE Budget (W)": 370,
-            "Uplink Speed (Gbps)": [10],
-            "Uplink Ports": 4,
+            "PoE Budget": 370,
+            "Uplink": [
+                {"Ports": 4, "Speed": [10]}
+            ],
+            "Switching Capacity": 146,
+            "Stacking": "No",
             "SKU": "MS130-24X-HW",
-            "Switching Capacity (Gbps)": 146,
+            "License": [
+                {"Enterprise": "LIC-MS130-24-xY", "Advanced": "LIC-MS130-24A-xY"}
+            ],
             "Datasheet": "https://documentation.meraki.com/MS/MS_Overview_and_Specifications/MS130_Datasheet",
             "Installation Guide": "https://documentation.meraki.com/MS/MS_Installation_Guides/MS130_Series_Installation_Guide"
         },
         "MS130-48X": {
             "Type": "L2",
-            "Access Ports": 48,
-            "Port Speed": [
-                {"Ports": 8, "Speed (Gbps)": [2.5]}, 
-                {"Ports": 40, "Speed (Gbps)": [1]}
+            "Access": [
+                {"Ports": 8, "Speed": [2.5]}, 
+                {"Ports": 40, "Speed": [1]}
             ],
             "PoE Type": "PoE+",
-            "PoE Budget (W)": 740,
-            "Uplink Speed (Gbps)": [10],
-            "Uplink Ports": 4,
+            "PoE Budget": 740,
+            "Uplink": [
+                {"Ports": 4, "Speed": [10]}
+            ],
+            "Switching Capacity": 200,
+            "Stacking": "No",
             "SKU": "MS130-48X-HW",
-            "Switching Capacity (Gbps)": 200,
+            "License": [
+                {"Enterprise": "LIC-MS130-48-xY", "Advanced": "LIC-MS130-48A-xY"}
+            ],
             "Datasheet": "https://documentation.meraki.com/MS/MS_Overview_and_Specifications/MS130_Datasheet",
             "Installation Guide": "https://documentation.meraki.com/MS/MS_Installation_Guides/MS130_Series_Installation_Guide"
         }
     },
+    "Meraki MS150": {
+        "MS150-24P-4X": {
+            "Type": "L2",
+            "Access": [
+                {"Ports": 24, "Speed": [1]}
+            ],
+            "PoE Type": "PoE+",
+            "PoE Budget": 370,
+            "Uplink": [
+                {"Ports": 4, "Speed": [10]}
+            ],
+            "Switching Capacity": 128,
+            "Stacking": "Physical",
+            "SKU": "MS150-24P-4X-HW",
+            "License": [
+                {"Enterprise": "LIC-MS150-24-xY", "Advanced": "LIC-MS150-24A-xY"}
+            ],
+            "Datasheet": "https://documentation.meraki.com/MS/MS_Overview_and_Specifications/MS150_Datasheet",
+            "Installation Guide": "https://documentation.meraki.com/MS/MS_Installation_Guides/MS150_Series_Installation_Guide"
+        },
+        "MS150-48LP-4X": {
+            "Type": "L2",
+            "Access": [
+                {"Ports": 48, "Speed": [1]}
+            ],
+            "PoE Type": "PoE+",
+            "PoE Budget": 370,
+            "Uplink": [
+                {"Ports": 4, "Speed": [10]}
+            ],
+            "Switching Capacity": 176,
+            "Stacking": "Physical",
+            "SKU": "MS150-48LP-4X-HW",
+            "License": [
+                {"Enterprise": "LIC-MS150-48-xY", "Advanced": "LIC-MS150-48A-xY"}
+            ],
+            "Datasheet": "https://documentation.meraki.com/MS/MS_Overview_and_Specifications/MS150_Datasheet",
+            "Installation Guide": "https://documentation.meraki.com/MS/MS_Installation_Guides/MS150_Series_Installation_Guide"
+        },
+        "MS150-48FP-4X": {
+            "Type": "L2",
+            "Access": [
+                {"Ports": 48, "Speed": [1]}
+            ],
+            "PoE Type": "PoE+",
+            "PoE Budget": 740,
+            "Uplink": [
+                {"Ports": 4, "Speed": [10]}
+            ],
+            "Switching Capacity": 176,
+            "Stacking": "Physical",
+            "SKU": "MS150-48FP-4X-HW",
+            "License": [
+                {"Enterprise": "LIC-MS150-48-xY", "Advanced": "LIC-MS150-48A-xY"}
+            ],
+            "Datasheet": "https://documentation.meraki.com/MS/MS_Overview_and_Specifications/MS150_Datasheet",
+            "Installation Guide": "https://documentation.meraki.com/MS/MS_Installation_Guides/MS150_Series_Installation_Guide"
+        },
+        "MS150-24MP-4X": {
+            "Type": "L2",
+            "Access": [
+                {"Ports": 8, "Speed": [5]},
+                {"Ports": 16, "Speed": [1]}
+            ],
+            "PoE Type": "UPoE",
+            "PoE Budget": 370,
+            "Uplink": [
+                {"Ports": 4, "Speed": [10]}
+            ],
+            "Switching Capacity": 192,
+            "Stacking": "Physical",
+            "SKU": "MS150-24MP-4X-HW",
+            "License": [
+                {"Enterprise": "LIC-MS150-24-xY", "Advanced": "LIC-MS150-24A-xY"}
+            ],
+            "Datasheet": "https://documentation.meraki.com/MS/MS_Overview_and_Specifications/MS150_Datasheet",
+            "Installation Guide": "https://documentation.meraki.com/MS/MS_Installation_Guides/MS150_Series_Installation_Guide"
+        },
+        "MS150-48MP-4X": {
+            "Type": "L2",
+            "Access": [
+                {"Ports": 16, "Speed": [5]},
+                {"Ports": 32, "Speed": [1]}
+            ],
+            "PoE Type": "UPoE",
+            "PoE Budget": 740,
+            "Uplink": [
+                {"Ports": 4, "Speed": [10]}
+            ],
+            "Switching Capacity": 304,
+            "Stacking": "Physical",
+            "SKU": "MS150-48MP-4X-HW",
+            "License": [
+                {"Enterprise": "LIC-MS150-48-xY", "Advanced": "LIC-MS150-48A-xY"}
+            ],
+            "Datasheet": "https://documentation.meraki.com/MS/MS_Overview_and_Specifications/MS150_Datasheet",
+            "Installation Guide": "https://documentation.meraki.com/MS/MS_Installation_Guides/MS150_Series_Installation_Guide"
+        },
+    },
     "Cisco Catalyst C9300L-M": {
         "C9300L-24P-4X": {
             "Type": "L3",
-            "Access Ports": 24,
-            "Port Speed": [
-                {"Ports": 24, "Speed (Gbps)": [1]}
+            "Access": [
+                {"Ports": 24, "Speed": [1]}
             ],
             "PoE Type": "PoE+",
-            "PoE Budget (W)": 505,
-            "Uplink Speed (Gbps)": [10],
-            "Uplink Ports": 4,
+            "PoE Budget": 505,
+            "Uplink": [
+                {"Ports": 4, "Speed": [10]}
+            ],
+            "Switching Capacity": 128,
+            "Stacking": "Physical",
             "SKU": "C9300L-24P-4X-M",
-            "Switching Capacity (Gbps)": 128,
+            "License": [
+                {"Enterprise": "LIC-C9300-24E-xY", "Advanced": "LIC-C9300-24A-xY"}
+            ],
             "Datasheet": "https://documentation.meraki.com/MS/MS_Overview_and_Specifications/Catalyst_9300L-M_Datasheet",
             "Installation Guide": "https://documentation.meraki.com/MS/MS_Installation_Guides/Catalyst_9300-M_Series_Installation_Guide"
         },
         "C9300L-24UXG-4X": {
             "Type": "L3",
-            "Access Ports": 24,
-            "Port Speed": [
-                {"Ports": 8, "Speed (Gbps)": [1, 2.5, 5, 10]}, 
-                {"Ports": 16, "Speed (Gbps)": [1]}
+            "Access": [
+                {"Ports": 8, "Speed": [1, 2.5, 5, 10]}, 
+                {"Ports": 16, "Speed": [1]}
             ],
             "PoE Type": "UPoE",
-            "PoE Budget (W)": 880,
-            "Uplink Speed (Gbps)": [10],
-            "Uplink Ports": 4,
+            "PoE Budget": 880,
+            "Uplink": [
+                {"Ports": 4, "Speed": [10]}
+            ],
+            "Switching Capacity": 272,
+            "Stacking": "Physical",
             "SKU": "C9300L-24UXG-4X-M",
-            "Switching Capacity (Gbps)": 272,
+            "License": [
+                {"Enterprise": "LIC-C9300-24E-xY", "Advanced": "LIC-C9300-24A-xY"}
+            ],
             "Datasheet": "https://documentation.meraki.com/MS/MS_Overview_and_Specifications/Catalyst_9300L-M_Datasheet",
             "Installation Guide": "https://documentation.meraki.com/MS/MS_Installation_Guides/Catalyst_9300-M_Series_Installation_Guide"
         },
         "C9300L-48P-4X": {
             "Type": "L3",
-            "Access Ports": 48,
-            "Port Speed": [
-                {"Ports": 48, "Speed (Gbps)": [1]}
+            "Access": [
+                {"Ports": 48, "Speed": [1]}
             ],
             "PoE Type": "PoE+",
-            "PoE Budget (W)": 505,
-            "Uplink Speed (Gbps)": [10],
-            "Uplink Ports": 4,
+            "PoE Budget": 505,
+            "Uplink": [
+                {"Ports": 4, "Speed": [10]}
+            ],
+            "Switching Capacity": 176,
+            "Stacking": "Physical",
             "SKU": "C9300L-48P-4X-M",
-            "Switching Capacity (Gbps)": 176,
+            "License": [
+                {"Enterprise": "LIC-C9300-48E-xY", "Advanced": "LIC-C9300-48A-xY"}
+            ],
             "Datasheet": "https://documentation.meraki.com/MS/MS_Overview_and_Specifications/Catalyst_9300L-M_Datasheet",
             "Installation Guide": "https://documentation.meraki.com/MS/MS_Installation_Guides/Catalyst_9300-M_Series_Installation_Guide"
         },
         "C9300L-48PF-4X": {
             "Type": "L3",
-            "Access Ports": 48,
-            "Port Speed": [
-                {"Ports": 48, "Speed (Gbps)": [1]}
+            "Access": [
+                {"Ports": 48, "Speed": [1]}
             ],
             "PoE Type": "PoE+",
-            "PoE Budget (W)": 890,
-            "Uplink Speed (Gbps)": [10],
-            "Uplink Ports": 4,
+            "PoE Budget": 890,
+            "Uplink": [
+                {"Ports": 4, "Speed": [10]}
+            ],
+            "Switching Capacity": 176,
+            "Stacking": "Physical",
             "SKU": "C9300L-48PF-4X-M",
-            "Switching Capacity (Gbps)": 176,
+            "License": [
+                {"Enterprise": "LIC-C9300-48E-xY", "Advanced": "LIC-C9300-48A-xY"}
+            ],
             "Datasheet": "https://documentation.meraki.com/MS/MS_Overview_and_Specifications/Catalyst_9300L-M_Datasheet",
             "Installation Guide": "https://documentation.meraki.com/MS/MS_Installation_Guides/Catalyst_9300-M_Series_Installation_Guide"
         },
         "C9300L-48UXG-4X": {
             "Type": "L3",
-            "Access Ports": 48,
-            "Port Speed": [
-                {"Ports": 12, "Speed (Gbps)": [1, 2.5, 5, 10]}, 
-                {"Ports": 36, "Speed (Gbps)": [1]}
+            "Access": [
+                {"Ports": 12, "Speed": [1, 2.5, 5, 10]}, 
+                {"Ports": 36, "Speed": [1]}
             ],
             "PoE Type": "UPoE",
-            "PoE Budget (W)": 675,
-            "Uplink Speed (Gbps)": [10],
-            "Uplink Ports": 4,
+            "PoE Budget": 675,
+            "Uplink": [
+                {"Ports": 4, "Speed": [10]}
+            ],
+            "Switching Capacity": 392,
+            "Stacking": "Physical",
             "SKU": "C9300L-48UXG-4X-M",
-            "Switching Capacity (Gbps)": 392,
+            "License": [
+                {"Enterprise": "LIC-C9300-48E-xY", "Advanced": "LIC-C9300-48A-xY"}
+            ],
             "Datasheet": "https://documentation.meraki.com/MS/MS_Overview_and_Specifications/Catalyst_9300L-M_Datasheet",
             "Installation Guide": "https://documentation.meraki.com/MS/MS_Installation_Guides/Catalyst_9300-M_Series_Installation_Guide"
         }
     }
 }
 
+switch_index = {
+    model: details
+    for family in SWITCH_MODELS.values()
+    for model, details in family.items()
+}
+
 def get_switch(model: str):
-    """Retrieve switch details based on the model."""
-    for family, switches in SWITCH_MODELS.items():
-        if model in switches:
-            return switches[model]
-    return None  # Return None if the model is not found
+    """Retrieve switch details using the pre-built index"""
+    return switch_index.get(model)
